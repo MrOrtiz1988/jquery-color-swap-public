43d1a38ab6fcfccfa69609c594f85c4ab86e1e7e
Luis Ortiz <l.ortizintec@gmail.com>
2023-04-03 15:03:14 -0400

    completed challenge

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ let num = 0;
 
 function onReady(){
     $('.generate-btn').on('click', handleGenerate);
-    $('.container').on('click', '.turn-yellow', turnYellow)
+    $('.container').on('click', '.turn-yellow', turnYellow);
+    $('.container').on('click', '.delete-btn', deleteThis);
 }
 
 function handleGenerate(){
@@ -17,11 +18,15 @@ function handleGenerate(){
     <div class="new-div">
         <p>${num}</p>
         <button class="turn-yellow">Yellow</button>
-        <button>Delete</button>
+        <button class="delete-btn">Delete</button>
     </div>
     `);
 }
 
 function turnYellow(){
     $(this).parent().css('background-color', 'yellow');
+}
+
+function deleteThis(){
+    $(this).parent().remove();
 }
