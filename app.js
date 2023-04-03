7f01258d7e322a716aa6ad0e63ad54659943b833
Luis Ortiz <l.ortizintec@gmail.com>
2023-04-03 14:53:03 -0400

    completed step 4

--- app.js
+++ app.js
@@ -2,13 +2,23 @@
 
 $(document).ready(onReady);
 
+
+let num = 0;
+
 function onReady(){
     console.log('jq working');
     $('.generate-btn').on('click', handleGenerate);
 }
 
 function handleGenerate(){
+console.log('being clicked');
+    num++;
+
     $('.container').append(`
-    <div class="new-div"></div>
-    `)
+    <div class="new-div">
+        <p>${num}</p>
+        <button>Yellow</button>
+        <button>Delete</button>
+    </div>
+    `);
 }
